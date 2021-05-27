e40a66689057e0b54b8ad7b0c58f06337b8cba3d
Rohit Patil <rahulhimesh09@gmail.com>
2021-05-27 23:13:21 +0530

    [ADD] Day 1's Second Solution
    if in for-in

---
+++ day1-2.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for (var char in s) {
+        char= s[char];
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char == 'u') {
+            console.log(char);
+        }
+    }
+    for (var char in s) {
+        char = s[char];
+        if (!(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char == 'u')) {
+            console.log(char);
+        }
+    }
+        
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
